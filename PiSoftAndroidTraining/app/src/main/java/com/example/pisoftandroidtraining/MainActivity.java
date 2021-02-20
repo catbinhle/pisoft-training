eec6438ad49b0582f0bc4ae130d318b5e7eb7e78
Cat Binh Le <catbinhle@gmail.com>
2021-02-20 10:29:46 +0700

    [PiSoft - Train] added main files

---
+++ PiSoftAndroidTraining/app/src/main/java/com/example/pisoftandroidtraining/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.pisoftandroidtraining;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+}
